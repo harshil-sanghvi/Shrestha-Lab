bf05d40a2dd67617014c1e7a969b5a8e7780e781
Harshil Sanghvi <harshil1708@gmail.com>
2024-09-22 21:35:55 -0400

    Added arguments parsing

--- ptc_staggered_ff.py
+++ ptc_staggered_ff.py
@@ -1,5 +1,6 @@
 import ptc_ff
 import pandas as pd
+import argparse
 
 class StaggeredFF(ptc_ff.FreezeFrame):
     def __init__(self, staggered_timestamps_path, *args, **kwargs):
@@ -15,21 +16,39 @@ class StaggeredFF(ptc_ff.FreezeFrame):
 
         current_key = None
         for row in df.itertuples(index=False):
-            experiment, animal_id, start_delay = row
+            experiment, animal_id, delay = row
 
             if isinstance(experiment, str):
                 current_key = experiment.lower()
                 if current_key not in self.staggered_timestamps:
                     self.staggered_timestamps[current_key] = {}
             elif current_key:
-                self.staggered_timestamps[current_key][animal_id] = start_delay
+                self.staggered_timestamps[current_key][animal_id] = delay
 
     def get_ff_avg(self, animal_id, start, end, ff_df):
-        start_delay = self.staggered_timestamps[self.experiment_name][animal_id]
-        return super().get_ff_avg(animal_id, start_delay, end, ff_df)
+        delay = self.staggered_timestamps[self.experiment_name][animal_id]
+        return super().get_ff_avg(animal_id, start + delay, end + delay, ff_df)
 
-ff = StaggeredFF(staggered_timestamps_path='/Users/harshil/Library/CloudStorage/GoogleDrive-Harshil.Sanghvi@stonybrook.edu/Shared drives/NBB_ShresthaLab_SharedDrive/LM - Harshil Sanghvi/New Data_ready for code/PTC_Staggered/PL_Rc_RmEngram.O4E PTC Freezeframe/20240829_PL_Rc_RmEngram.O4E CT5 PTC/20240829_PL_RC_RmEngram.O4E CT5 PTC Start_timestamps.xlsx', output_path='./', ct_path='/Users/harshil/Library/CloudStorage/GoogleDrive-Harshil.Sanghvi@stonybrook.edu/Shared drives/NBB_ShresthaLab_SharedDrive/LM - Harshil Sanghvi/New Data_ready for code/PTC_Staggered/PL_Rc_RmEngram.O4E PTC Freezeframe/PL_Rc_RmEngram.O4E Cohorts.xlsx', folder_path='/Users/harshil/Library/CloudStorage/GoogleDrive-Harshil.Sanghvi@stonybrook.edu/Shared drives/NBB_ShresthaLab_SharedDrive/LM - Harshil Sanghvi/New Data_ready for code/PTC_Staggered/PL_Rc_RmEngram.O4E PTC Freezeframe', timestamps_path='/Users/harshil/Library/CloudStorage/GoogleDrive-Harshil.Sanghvi@stonybrook.edu/Shared drives/NBB_ShresthaLab_SharedDrive/LM - Harshil Sanghvi/New Data_ready for code/PTC_Staggered/PTC Timestamps.xlsx')
-ff.process_staggered_timestamps()
-ff.process_timestamps()
-ff.process_folder()
-# print(ff.staggered_timestamps)
+def main():
+    '''Function to parse the command line arguments and process the FreezeFrame data.'''
+    parser = argparse.ArgumentParser(description='Process FreezeFrame data')
+    parser.add_argument('--timestamps', type=str, required=True, help='Path to the timestamps file')
+    parser.add_argument('--ct', type=str, required=True, help='Path to the CT file')
+    parser.add_argument('--folder', type=str, required=True, help='Path to the folder containing the FreezeFrame data')
+    parser.add_argument('--output', type=str, required=True, help='Path to the output folder')
+    parser.add_argument('--staggered_timestamps', type=str, required=True, help='Path to the staggered timestamps file')
+    args = parser.parse_args()
+    timestamps_path = args.timestamps
+    ct_path = args.ct
+    folder_path = args.folder
+    output_path = args.output
+    staggered_timestamps_path = args.staggered_timestamps
+
+    ff = StaggeredFF(staggered_timestamps_path, timestamps_path, ct_path, folder_path, output_path) # create staggered FF object
+    ff.process_staggered_timestamps() # process the staggered timestamps file
+    ff.process_timestamps() # process the timestamps file
+    ff.process_folder() # process the folder containing the FreezeFrame data
+
+# Run the main function
+if __name__ == '__main__':
+    main()
